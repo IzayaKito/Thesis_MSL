dfdc54703cb273c0f32e219a472840cf8c9266d7
IzayaKito <juanpa1920@gmail.com>
2020-09-14 14:27:42 -0500

    modified:   LiTS_getDatabase_unet.py 	modified:   image_util_unet.py 	modified:   models/networks_unet.py

--- LiTS_getDatabase_unet.py
+++ LiTS_getDatabase_unet.py
@@ -79,7 +79,8 @@ class DataProvider_LiTS(BaseDataProvider):
 		#		labels[:, :, ich] = file['imdb']['labels'][0][0]
 
 		fileName = os.path.join(filePath[0], str(filePath[1]) + '.jpg')
-		data = io.imread(fileName)
+		data = io.imread(fileName,plugin='matplotlib')
+		print('Shape after reading',data.shape)
 		#data = np.clip(data+124, 0, 400)
 		
 		path = filePath[0] + str(filePath[1])
